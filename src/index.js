5b543c7891d311dc62a9b5372eb209f70a2c7414
marina <marina.shatinskaya@gmail.com>
2018-02-23 20:34:37 +0300

    solution-test 2

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ module.exports = function getZerosCount(number, base) {
   'use strict';
 
   var test = base;
-  for (var i=2; i<=base/2, test >i ; i++){
-	  while (test%i === 0) {
+  for (var i=2; i<=base/2; i++){
+	  while ((test%i === 0)&& (test >i)) {
 	    test = test/i;
 	  }
   }	
   base = test;
 
   var k=0; 
-  for (var i=base; i<=number; i*=base){  
-    k+=parseInt(number/i);  
+  for (var i=base; i<=number; i*=base){
+    if (number!=i){
+      k+=parseInt(number/i); 
+    }  
   }
   return k;
 }
